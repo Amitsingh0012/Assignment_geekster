fde9b59a343ddeb03c298f9a736aad1302726a48
Amit Singh <109969844+Amitsingh0012@users.noreply.github.com>
2023-07-15 22:01:17 +0530

    Add files via upload

---
+++ Restaurant/Restaurant/src/main/java/com/Restaurant/Restaurant/entity/User.java
@@ -0,0 +1,24 @@
+package com.Restaurant.Restaurant.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    @Email
+    private String email;
+    private String roles;
+}
+
